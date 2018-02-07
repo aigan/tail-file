90832984993d35a1ccf8ade63bca42abc766d679
Jonas Liljegren <jonas@avisita.com>
2018-02-07 10:40:51 +0100

    indentation

--- tail.js
+++ tail.js
@@ -120,221 +120,221 @@ class Tail extends EventEmitter {
 	}
 
 
-start(){
-	return this.tryTail( this.filename ).catch( err =>{
-		if( err.code !== 'ENOENT' ) throw err;
-		this.err1 = err;
-		
-		debug(`${this.filename} gone. Trying ${this.secondary}`);
-		return this.tryTail( this.secondary );
-	}).catch( err =>{
-		if( this.err1 ){ // report error for the primary file
-			this.onError( this.err1 );
-			delete this.err1;
-		} else {
-			this.onError( err );
-		}
+	start(){
+		return this.tryTail( this.filename ).catch( err =>{
+			if( err.code !== 'ENOENT' ) throw err;
+			this.err1 = err;
+			
+			debug(`${this.filename} gone. Trying ${this.secondary}`);
+			return this.tryTail( this.secondary );
+		}).catch( err =>{
+			if( this.err1 ){ // report error for the primary file
+				this.onError( this.err1 );
+				delete this.err1;
+			} else {
+				this.onError( err );
+			}
 
-		if( this.force ){
-			this.started = this.filename;
+			if( this.force ){
+				this.started = this.filename;
+				this.watchForPrimary();
+			}
+		});
+	}
+
+	async tryTail( filename ){
+		this.started = filename;
+		this.watcher = fs.watch(filename );
+		fs.stat(filename, this.getStat.bind(this) );
+		this.readStuff(); // Do not wait in case we don't start at the end
+
+		this.watcher.on('change', this.readStuff.bind(this) ); // even if rename
+		this.watcher.on('error', this.onError.bind(this) );
+
+		if( filename == this.secondary ){
+			this.emit('secondary', this.secondary);
 			this.watchForPrimary();
 		}
-	});
-}
-
-async tryTail( filename ){
-	this.started = filename;
-	this.watcher = fs.watch(filename );
-	fs.stat(filename, this.getStat.bind(this) );
-	this.readStuff(); // Do not wait in case we don't start at the end
-
-	this.watcher.on('change', this.readStuff.bind(this) ); // even if rename
-	this.watcher.on('error', this.onError.bind(this) );
-
-	if( filename == this.secondary ){
-		this.emit('secondary', this.secondary);
-		this.watchForPrimary();
-	}
-}
-
-watchForPrimary(){
-	if( this.dirwatcher ) return;
-
-	debug(`Start watching for the return of ${this.filename}`);
-
-	const dirname = path.dirname( this.filename );
-
-	this.dirwatcher = fs.watch( dirname );
-	this.dirwatcher.on('change', this.checkDir.bind(this) );
-	this.dirwatcher.on('error', this.onError.bind(this) );
-}
-
-checkDir(type, name ){
-	//debug(`Dir ${type}: ${name}`);
-	if( name !== this.filename ) return;
-	debug(`Dir ${type}: ${name}`);
-
-	// It will decide if it's time to switch over
-	if( this.fd ) return this.readStuff();
-
-	this.stop();
-	this.startPos = 'start';
-	this.emit('restart','PRIMEFOUND');
-	return setImmediate( this.start.bind(this) );
-}
-
-onError( err ){
-	// handle all file and watcher errors
-	this.stop();
-	this.emit( 'error', err );
-	debug( err );
-}
-
-stop(){
-	debug(`Stops tail of ${this.started}`);
-	if( this.dirwatcher ){
-		this.dirwatcher.close();
-		delete this.dirwatcher;
-	}
-	
-	if( this.watcher ){
-		this.watcher.close();
-		delete this.watcher;
 	}
 
-	if( this.fd ){
-		if( this.fd !==  'init' ){
-			debug("Closing fd " + this.fd);
-			fs.close( this.fd, err =>{
-				if( err ) return debug( "closing1 " + err );
-			});
-		}
-		delete this.fd;
+	watchForPrimary(){
+		if( this.dirwatcher ) return;
+
+		debug(`Start watching for the return of ${this.filename}`);
+
+		const dirname = path.dirname( this.filename );
+
+		this.dirwatcher = fs.watch( dirname );
+		this.dirwatcher.on('change', this.checkDir.bind(this) );
+		this.dirwatcher.on('error', this.onError.bind(this) );
 	}
 
-	this.started = null;
-}
+	checkDir(type, name ){
+		//debug(`Dir ${type}: ${name}`);
+		if( name !== this.filename ) return;
+		debug(`Dir ${type}: ${name}`);
 
-getStat( err, stats ){
-	if( err ) return this.onError( err );
+		// It will decide if it's time to switch over
+		if( this.fd ) return this.readStuff();
 
-	let start = stats.size;
-	if( typeof this.startPos == 'number' ){
-		if( this.startPos <= stats.size ) start = this.startPos;
-	} else if( this.startPos == 'start' ){
-		if( stats.size <this.cutoff ){
-			start = 0;
-		} else {
-			this.emit('skip',start);
-		}
+		this.stop();
+		this.startPos = 'start';
+		this.emit('restart','PRIMEFOUND');
+		return setImmediate( this.start.bind(this) );
 	}
 
-	this.pos = start;
-	this.ino = stats.ino;
-	debug(`Starting tail at ${this.pos}`);
-}
-
-createReader(){
-	if( this.fd ) return;
-	this.fd = 'init';
-	fs.open(this.started,'r', (err,fd) =>{
-		if(err) return this.onError( err );
-		if(!this.fd){ // in the middle of stop()
-			debug("Closing2 fd " + this.fd);
-			fs.close( fd, err =>{
-				if( err ) return debug( "closing " + err );
-			});
-			return;
-		}
-		debug("Opened file as fd " + fd);
-		this.emit('ready', fd);
-		this.fd = fd;
-		this.readStuff();
-	});
-}
-
-readStuff(){
-	if( !this.started ) return;
-	this.createReader();
-	if( this.fd == 'init' ) return;
-	if( this.pos == 'init' ) return;
-	if( this.reading ) return;
-	this.reading = true;
-
-	//debug("Starts reading at " + this.pos);
-	//const bufsize = 6; for tests
-	const bufsize = 4096;
-	if(!this.buf) this.buf = Buffer.alloc(bufsize);
-	fs.read( this.fd, this.buf, 0, bufsize, this.pos, this.parseStuff.bind(this) );
-}
-
-parseStuff(err, bytesRead, buf ){
-	this.reading = false;
-	if( err ) return this.onError( err );
-
-	let data;
-	if( bytesRead == 0 ){
-		data = this.decoder.end(); // might have partial characters
-	} else {
-		this.pos += bytesRead;
-		//debug(`Read ${bytesRead} bytes`);
-		data = this.decoder.write( buf.slice(0,bytesRead) );
+	onError( err ){
+		// handle all file and watcher errors
+		this.stop();
+		this.emit( 'error', err );
+		debug( err );
 	}
 
-	// Everything else comes down to this line
-	const newLines = (this.txt+data).split(this.sep);
-
-	this.txt = newLines.pop();
-
-	for( let line of newLines ){
-		this.emit('line', line );
-	}
-
-	if( bytesRead == 0 ) return this.onEndOfFile();
-
-	setImmediate( this.readStuff.bind(this) );
-}
-
-onEndOfFile(){
-	//debug("End of file\n");
-	//debug("Should we switch the streams?");
-	fs.stat(this.filename, (err,stat) =>{
-		if( err ){
-			if( debug.enabled ){
-				if( err.code == 'ENOENT' ){
-					if( !this.dirwatcher ) debug(`${this.filename} does not exist`);
-				}
-				else debug(err);
-			}
-			
-			this.watchForPrimary(); // usually the right response
-			return;
+	stop(){
+		debug(`Stops tail of ${this.started}`);
+		if( this.dirwatcher ){
+			this.dirwatcher.close();
+			delete this.dirwatcher;
 		}
 		
-		if( this.ino != stat.ino ){
-			if( stat.size ){
-				debug("Switching over to the new file");
+		if( this.watcher ){
+			this.watcher.close();
+			delete this.watcher;
+		}
+
+		if( this.fd ){
+			if( this.fd !==  'init' ){
+				debug("Closing fd " + this.fd);
+				fs.close( this.fd, err =>{
+					if( err ) return debug( "closing1 " + err );
+				});
+			}
+			delete this.fd;
+		}
+
+		this.started = null;
+	}
+
+	getStat( err, stats ){
+		if( err ) return this.onError( err );
+
+		let start = stats.size;
+		if( typeof this.startPos == 'number' ){
+			if( this.startPos <= stats.size ) start = this.startPos;
+		} else if( this.startPos == 'start' ){
+			if( stats.size <this.cutoff ){
+				start = 0;
+			} else {
+				this.emit('skip',start);
+			}
+		}
+
+		this.pos = start;
+		this.ino = stats.ino;
+		debug(`Starting tail at ${this.pos}`);
+	}
+
+	createReader(){
+		if( this.fd ) return;
+		this.fd = 'init';
+		fs.open(this.started,'r', (err,fd) =>{
+			if(err) return this.onError( err );
+			if(!this.fd){ // in the middle of stop()
+				debug("Closing2 fd " + this.fd);
+				fs.close( fd, err =>{
+					if( err ) return debug( "closing " + err );
+				});
+				return;
+			}
+			debug("Opened file as fd " + fd);
+			this.emit('ready', fd);
+			this.fd = fd;
+			this.readStuff();
+		});
+	}
+
+	readStuff(){
+		if( !this.started ) return;
+		this.createReader();
+		if( this.fd == 'init' ) return;
+		if( this.pos == 'init' ) return;
+		if( this.reading ) return;
+		this.reading = true;
+
+		//debug("Starts reading at " + this.pos);
+		//const bufsize = 6; for tests
+		const bufsize = 4096;
+		if(!this.buf) this.buf = Buffer.alloc(bufsize);
+		fs.read( this.fd, this.buf, 0, bufsize, this.pos, this.parseStuff.bind(this) );
+	}
+
+	parseStuff(err, bytesRead, buf ){
+		this.reading = false;
+		if( err ) return this.onError( err );
+
+		let data;
+		if( bytesRead == 0 ){
+			data = this.decoder.end(); // might have partial characters
+		} else {
+			this.pos += bytesRead;
+			//debug(`Read ${bytesRead} bytes`);
+			data = this.decoder.write( buf.slice(0,bytesRead) );
+		}
+
+		// Everything else comes down to this line
+		const newLines = (this.txt+data).split(this.sep);
+
+		this.txt = newLines.pop();
+
+		for( let line of newLines ){
+			this.emit('line', line );
+		}
+
+		if( bytesRead == 0 ) return this.onEndOfFile();
+
+		setImmediate( this.readStuff.bind(this) );
+	}
+
+	onEndOfFile(){
+		//debug("End of file\n");
+		//debug("Should we switch the streams?");
+		fs.stat(this.filename, (err,stat) =>{
+			if( err ){
+				if( debug.enabled ){
+					if( err.code == 'ENOENT' ){
+						if( !this.dirwatcher ) debug(`${this.filename} does not exist`);
+					}
+					else debug(err);
+				}
+				
+				this.watchForPrimary(); // usually the right response
+				return;
+			}
+			
+			if( this.ino != stat.ino ){
+				if( stat.size ){
+					debug("Switching over to the new file");
+					this.stop();
+					this.startPos = 'start';
+					this.emit('restart','NEWPRIME');
+					return setImmediate( this.start.bind(this) );
+				}
+
+				// Wait until it has something to read
+				return this.watchForPrimary();
+
+			} else if( stat.size < this.pos ){
+				debug("File truncated");
 				this.stop();
 				this.startPos = 'start';
-				this.emit('restart','NEWPRIME');
+				this.emit('restart','TRUNCATE');
 				return setImmediate( this.start.bind(this) );
 			}
 
-			// Wait until it has something to read
-			return this.watchForPrimary();
+			this.emit('eof', this.pos);
+		});
 
-		} else if( stat.size < this.pos ){
-			debug("File truncated");
-			this.stop();
-			this.startPos = 'start';
-			this.emit('restart','TRUNCATE');
-			return setImmediate( this.start.bind(this) );
-		}
-
-		this.emit('eof', this.pos);
-	});
-
-}
+	}
 }
 
 module.exports = Tail;
